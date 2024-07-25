45ee1a69b505aa9d52f19945afc4aaaabe417a69
MGfimka <efimoffmg@yandex.ru>
2024-07-25 18:58:55 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,20 @@
+#"1st programs"
+a = 9
+b = 0.5
+c = 5
+print((a**b)*c)
+#"2st programs"
+a = 9.99
+b = 9.98
+c = 1000
+d = 1000.1
+print(a>b and c!=d)
+#"3st programs"
+a = 1234
+b = 5678
+print(a%1000//10+b%1000//10)
+#"4st programs"
+a = 13.42
+b = 42.13
+print((a*100%100)==(b*100//100) or (a*100//100)==(b*100%100))
+
